ad326a831853424c4622936730d83358d0e0f922
Chris Carr <christocarr@outlook.com>
2020-05-02 12:21:09 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+import redux from 'redux'
+
+function increment() {
+  return {
+    type: "INCREMENT"
+  }
+}
+
+function decrement() {
+  return {
+    type: "DECREMENT"
+  }
+}
+
+function double() {
+  return {
+    type: "DOUBLE"
+  }
+}
+
+function halve() {
+  return {
+    type: "HALVE"
+  }
+}
+
+function reducer(state={count: 0}, action) {
+  switch(action.type) {
+    case "INCREMENT": 
+      return {
+        count: state.count + 1
+      }
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      }
+    case "DOUBLE":
+      return {
+        count: state.count * 2
+      }
+    case "HALVE":
+      return {
+        count: Math.round(state.count / 2)
+      }
+    default: 
+      return state
+  }
+}

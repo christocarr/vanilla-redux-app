768ec424ef3ed74387b485a5bcbd9e597b1917e2
Chris Carr <christocarr@outlook.com>
2020-05-03 11:37:27 +0100

    Remove unused code and import store

--- main.js
+++ main.js
@@ -1,138 +1,90 @@
-const redux = require('redux');
+import store from './redux'
 
-const initialState = {
-  count: 0,
-  favoriteThings: [],
-  youTubeVideo: {
-    title: "",
-    viewCount: 0,
-    votes: {
-      up: 0,
-      down: 0
-    }
-  }
-};
+console.log(store)
+// const redux = require('redux');
 
-console.log(initialState)
+// const initialState = {
+//   count: 0,
+//   favoriteThings: [],
+//   youTubeVideo: {
+//     title: "",
+//     viewCount: 0,
+//     votes: {
+//       up: 0,
+//       down: 0
+//     }
+//   }
+// };
 
-function changeCount(amount) {
-  return {
-    type: 'CHANGE_COUNT',
-    payload: amount,
-  };
-}
+// console.log(initialState)
 
-function addFavoriteThing(thing) {
-  return {
-    type: 'ADD_FAVORITE_THING',
-    payload: thing
-  }
-}
+// function reducer(state = initialState, action) {
+//   switch (action.type) {
+//     case 'ADD_FAVORITE_THING': 
+//       return {
+//         ...state,
+//         favoriteThings: [...state.favoriteThings, action.payload]
+//       }
+//     case 'REMOVE_FAVORITE_THING':
+//       const newFavoriteThings = state.favoriteThings.filter(thing => thing !== action.payload)
+//       return {
+//         ...state,
+//         favoriteThings: newFavoriteThings
+//       }
+//     case 'ADD_YOUTUBE_TITLE': 
+//       return {
+//         ...state,
+//         youTubeVideo: {
+//           ...state.youTubeVideo,
+//           title: action.payload
+//         }
+//       }
+//     case 'UP_VOTE_VIDEO':
+//       return {
+//         ...state,
+//         youTubeVideo: {
+//           ...state.youTubeVideo,
+//           votes: {
+//             ...state.youTubeVideo.votes,
+//             up: state.youTubeVideo.votes.up + 1
+//           }
+//         }
+//       }
+//     case 'DOWN_VOTE_VIDEO': 
+//       return {
+//         ...state,
+//         youTubeVideo: {
+//           ...state.youTubeVideo,
+//           viewCount: state.youTubeVideo.viewCount + 1,
+//           votes: {
+//             ...state.youTubeVideo.votes,
+//             down: state.youTubeVideo.votes.down + 1
+//           }
+//         }
+//       }
+//     case 'INCREMENT_VIDEO_VIEW_COUNT':
+//       return {
+//         ...state,
+//         youTubeVideo: {
+//           ...state.youTubeVideo,
 
-function removeFavoriteThing(thing) {
-  return {
-    type: 'REMOVE_FAVORITE_THING',
-    payload: thing
-  }
-}
+//           votes: {
+//             ...state.youTubeVideo.votes,
+//           }
+//         }
+//       }
+//     default:
+//       return state;
+//   }
+// }
 
-function addYouTubeTitle(title) {
-  return {
-    type: 'ADD_YOUTUBE_TITLE',
-    payload: title
-  }
-}
+// const store = redux.createStore(reducer);
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
 
-function upVoteVideo() {
-  return {
-    type: 'UP_VOTE_VIDEO'
-  }
-}
+// store.dispatch(upVoteVideo());
+// store.dispatch(downVoteVideo());
 
-function downVoteVideo() {
-  return {
-    type: 'DOWN_VOTE_VIDEO'
-  }
-}
-
-function incrementVideoViewCount() {
-  return {
-    type: 'INCREMENT_VIDEO_VIEW_COUNT'
-  }
-}
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case 'CHANGE_COUNT':
-      return {
-        ...state,
-        count: state.count + action.payload,
-      };
-    case 'ADD_FAVORITE_THING': 
-      return {
-        ...state,
-        favoriteThings: [...state.favoriteThings, action.payload]
-      }
-    case 'REMOVE_FAVORITE_THING':
-      const newFavoriteThings = state.favoriteThings.filter(thing => thing !== action.payload)
-      return {
-        ...state,
-        favoriteThings: newFavoriteThings
-      }
-    case 'ADD_YOUTUBE_TITLE': 
-      return {
-        ...state,
-        youTubeVideo: {
-          ...state.youTubeVideo,
-          title: action.payload
-        }
-      }
-    case 'UP_VOTE_VIDEO':
-      return {
-        ...state,
-        youTubeVideo: {
-          ...state.youTubeVideo,
-          votes: {
-            ...state.youTubeVideo.votes,
-            up: state.youTubeVideo.votes.up + 1
-          }
-        }
-      }
-    case 'DOWN_VOTE_VIDEO': 
-      return {
-        ...state,
-        youTubeVideo: {
-          ...state.youTubeVideo,
-          viewCount: state.youTubeVideo.viewCount + 1,
-          votes: {
-            ...state.youTubeVideo.votes,
-            down: state.youTubeVideo.votes.down + 1
-          }
-        }
-      }
-    case 'INCREMENT_VIDEO_VIEW_COUNT':
-      return {
-        ...state,
-        youTubeVideo: {
-          ...state.youTubeVideo,
-
-          votes: {
-            ...state.youTubeVideo.votes,
-          }
-        }
-      }
-    default:
-      return state;
-  }
-}
-
-const store = redux.createStore(reducer);
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-store.dispatch(upVoteVideo());
-store.dispatch(downVoteVideo());
-
-store.dispatch(incrementVideoViewCount())
+// store.dispatch(incrementVideoViewCount())
 

87e020bc3175bf432ee30b46d0a4a3aa51366540
Chris Carr <christocarr@outlook.com>
2020-05-03 11:11:45 +0100

    Add import to import reducer functions

--- redux/index.js
+++ redux/index.js
@@ -1,3 +1,5 @@
 const redux = require ('redux')
 const { combineReducers, createStore } = redux
-
+import countReducer from './count'
+import favoriteThingsReducer from './favoriteThings'
+import youTubeVideoReducer from './youTubeVideo'
